fb9da4603a4158ca1b1470b2e008d9b05d1ee460
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-04-12 13:37:14 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,21 +6,21 @@
 GREEN_BOLD="\033[1;32m"
 RESET="\033[0m"
 
-# Mensagem de atualização
-echo -e "${GREEN_BOLD}Baixando atualização para o sistema Sony PlayStation 3 \"ps3\"${RESET}"
-
 # Baixar o arquivo XPS3 para /usr/bin, tornar executável
 curl -sL https://raw.githubusercontent.com/JeversonDiasSilva/ps3_iso_para_pasta/main/XPS3 -o /usr/bin/XPS3 > /dev/null 2>&1
 chmod +x /usr/bin/XPS3 > /dev/null 2>&1
-
+clear
 # Substituir mimeapps.list
 rm -f /userdata/system/.config/mimeapps.list > /dev/null 2>&1
 curl -sL https://raw.githubusercontent.com/JeversonDiasSilva/ps3_iso_para_pasta/main/mimeapps.list -o /userdata/system/.config/mimeapps.list > /dev/null 2>&1
-
+clear
 # Substituir ou adicionar o userapp desktop
 DESKTOP_PATH="/userdata/system/.local/share/applications/userapp-XPS3-BFGL42.desktop"
 rm -f "$DESKTOP_PATH" > /dev/null 2>&1
 curl -sL https://raw.githubusercontent.com/JeversonDiasSilva/ps3_iso_para_pasta/main/userapp-XPS3-BFGL42.desktop -o "$DESKTOP_PATH" > /dev/null 2>&1
-
+clear
+# Mensagem de atualização
+echo -e "${GREEN_BOLD}Baixando atualização para o sistema Sony PlayStation 3 \"ps3\"${RESET}"
+sleep 5
 # Salvar overlay no Batocera
 batocera-save-overlay > /dev/null 2>&1
